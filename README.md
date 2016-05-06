15c296b5965dc05699d3e79ebe28d591213de284
hsinjung wu <hsinjungwu@gmail.com>
2016-05-06 13:55:11 +0800

    first add
    first add

---
+++ README.md
@@ -0,0 +1,24 @@
+#SystemwebMonthlyReport
+
+本 Add-In 僅提供[博暉科技](http://www.systemweb.com.tw/) 同仁快速撰寫每月工作日誌使用。
+
+##How to install
+
+1. 下載[最新版本](https://github.com/hsinjungwu/SystemwebMonthlyReport/releases/download/latest/SystemwebMonthlyReport.zip)。
+2. 解壓縮並執行 `setup.exe` 即可。
+
+##How to uninstall
+
+1. 進入 **控制台\所有控制台項目\程式和功能** 移除 `SystemwebMonthlyReport` 即可。
+
+##How to use
+
+安裝完後，可以看到 Outlook 多了一個 Ribbon 如下圖
+
+![畫面顯示](./readme/ribbon.png)
+
+點選各個按鈕即可在 **預設資料夾** 中產生工作日誌內容
+
+![畫面顯示](./readme/after_create.png)
+
+接著自行調整時數與專案分類，再拖曳至正確位置即可。
---
+++ ThisAddIn.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace SystemwebMonthlyReport
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon();
+        }
+
+        public void CreateTask(string subject)
+        {
+            Outlook.TaskItem task = Application.CreateItem(
+            Outlook.OlItemType.olTaskItem) as Outlook.TaskItem;
+
+            task.Subject = subject;
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            int day = DateTime.Now.Day;
+            task.StartDate = new DateTime(year, month, 1);
+            if (day < 20)
+            {
+                task.StartDate = task.StartDate.AddMonths(-1);
+            }
+            task.DueDate = task.StartDate.AddMonths(1).AddDays(-1);
+            task.Complete = true;
+            task.Mileage = task.StartDate.ToString("yyyy/MM/dd");
+            task.DateCompleted = task.DueDate;
+            task.Save();
+        }
+
+        #region VSTO 產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion VSTO 產生的程式碼
+    }
+}
